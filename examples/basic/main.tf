b54df3f30beef71080a9259b63cfbc8cac27b2f0
Leonardo Barros <115377071+Barrosleo@users.noreply.github.com>
2026-03-09 10:39:23 +0000

    Create main.tf

---
+++ examples/basic/main.tf
@@ -0,0 +1,9 @@
+```hcl
+provider "azuread" {}
+
+module "engineering_group" {
+  source       = "../../modules/security-group"
+  display_name = "GG-Engineering"
+  description  = "Engineering security group"
+}
+```
